0b1f92ac062c6b9fd681052e4c208af0cab9ff9e
Holger Müller <holger@mueller-gelff.de>
2018-09-10 20:01:02 +0200

    Configuration update

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -627,7 +627,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 3200, 500 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 3200, 250 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -874,7 +874,7 @@
 // @section extruder
 
 // For direct drive extruder v9 set to true, for geared extruder set to false.
-#define INVERT_E0_DIR false
+#define INVERT_E0_DIR true
 #define INVERT_E1_DIR false
 #define INVERT_E2_DIR false
 #define INVERT_E3_DIR false
